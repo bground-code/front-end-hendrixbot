074f1043be0fa68e98ccbe007e88e64c0bcd4701
caique <caiquelacerda15@gmail.com>
2024-05-06 19:41:10 -0300

    feat: criacao de chat

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChatService, MessageDTO } from '../../client/chat.service';
+import { Subscription } from 'rxjs';
+import { FormsModule } from "@angular/forms";
+import { CommonModule, NgClass, NgFor } from "@angular/common";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgClass,
+    NgFor,
+    CommonModule
+  ],
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+  messages: MessageDTO[] = [];
+  newMessage = '';
+  isServerThinking = false;
+
+  private messagesSubscription: Subscription | undefined;
+
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit() {
+    this.messagesSubscription = this.chatService.getMessages$().subscribe((message: MessageDTO) => {
+      setTimeout(() => {
+        this.messages.push(message); // Adiciona a mensagem recebida ao array após o delay
+        this.isServerThinking = false; // Desativa o indicador de "digitando"
+      }, 2000); // Delay simulando o servidor "digitando"
+    });
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      const msg: MessageDTO = { text: this.newMessage, type: 'sent' };
+      this.messages.push(msg);
+      this.chatService.sendMessage(this.newMessage);
+      this.newMessage = '';
+      this.isServerThinking = true;
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.messagesSubscription) {
+      this.messagesSubscription.unsubscribe();
+    }
+  }
+}
