06dff1ac8dfbffb870f052f024f20477aa0f2179
caique <caiquelacerda15@gmail.com>
2024-05-09 22:02:07 -0300

    feat: criacao de token interceptor

--- src/app/client/auth.service.ts
+++ src/app/client/auth.service.ts
@@ -7,6 +7,15 @@ export class AuthService {
   constructor() {}
 
   isLogado() {
-    return localStorage.getItem('dadosUsuario');
+    const dadosUsuario = localStorage.getItem('dadosUsuario');
+    if (dadosUsuario) {
+      const usuario = JSON.parse(dadosUsuario);
+      const token = usuario.tokenDto?.accessToken;
+      if (token) {
+        localStorage.setItem('accessToken', token);
+        return true;
+      }
+    }
+    return false;
   }
 }
