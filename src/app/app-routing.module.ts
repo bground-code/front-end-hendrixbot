ce2c9da3b4c105228f8039a898877da23e4fdf07
natalia-hnzn <natalia.hnzn@gmail.com>
2024-06-15 15:55:42 -0300

    fix: layout login; altera dir do componente dashboard, para responsividade com outros componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
-import {HomeComponent} from './components/home/home.component';
-import {CadastroUsuarioComponent} from './components/cadastro-usuario/cadastro-usuario/cadastro-usuario.component';
-import {IntentsComponent} from './components/intents/intents.component';
-import {StoryComponent} from "./components/story/story.component";
-import {NluComponent} from "./components/nlu/nlu.component";
-import {ChatComponent} from "./components/chat/chat.component";
-import {HistoricoComponent} from "./components/historico/historico.component";
-import {VisualizarConversaComponent} from "./components/historico/visualizar.component";
-import {AtendimentoComponent} from "./components/atendimento/atendimento.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario/cadastro-usuario.component';
+import { IntentsComponent } from './components/intents/intents.component';
+import { StoryComponent } from './components/story/story.component';
+import { NluComponent } from './components/nlu/nlu.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { HistoricoComponent } from './components/historico/historico.component';
+import { VisualizarConversaComponent } from './components/historico/visualizar.component';
+import { AtendimentoComponent } from './components/atendimento/atendimento.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'cadastro-usuario', component: CadastroUsuarioComponent},
-  {path: 'intents', component: IntentsComponent},
-  {path: 'historia', component: StoryComponent},
-  {path: 'nlu', component: NluComponent},
-  {path: 'chat', component: ChatComponent},
-  {path: 'historico', component: HistoricoComponent},
-  {path: 'visualizar-conversa/:sessionId', component: VisualizarConversaComponent},
-  {path: 'atendimento', component: AtendimentoComponent},
-  {path: 'relatorios', component: DashboardComponent},
-
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'cadastro-usuario', component: CadastroUsuarioComponent },
   { path: 'intents', component: IntentsComponent },
   { path: 'historia', component: StoryComponent },
@@ -35,20 +26,13 @@ const routes: Routes = [
     component: VisualizarConversaComponent,
   },
   { path: 'atendimento', component: AtendimentoComponent },
+  { path: 'relatorios', component: DashboardComponent },
 
+  // Default route
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
 
-  {
-    path: '',
-    component: ChatComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
+  // Wildcard route for handling 404 (Page Not Found)
+  { path: '**', redirectTo: '/chat' },
 ];
 
 @NgModule({
